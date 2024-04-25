0024be58a2661365102d59cb69d990dadbbd8a8b
daphnewitmer <daphne.witmer@clarify.nl>
2024-04-25 12:15:37 +0200

    update packages

--- rules/base.js
+++ rules/base.js
@@ -15,11 +15,12 @@ module.exports = {
 		],
 		"import/extensions": "off",
 
-		"dot-notation": "off",												// OFF: Access properties using the dot notation (foo.bar) instead of square-bracket notation (foo["bar"])
-		"no-multi-assign": ["error", { "ignoreNonDeclaration": true }], 	// Disallow use of chained assignment expressions (const foo = bar = 0;), allows chains that don’t include initializations
-		"no-param-reassign": ["error", { "props": false}],					// Disallow reassignment of function parameters, but allow modifying properties of parameters
-		"no-plusplus": "off",												// OFF: Disallow the unary operators ++ and --
-		"no-unused-vars": "warn", 											// No unused variables (eslint:recommended gives error)
-		"prefer-destructuring": "off",										// OFF: Prefer destructuring from arrays and objects
-		"prefer-arrow-callback": [ "error", { "allowNamedFunctions": true } ], // Prefer arrow callbacks over anonymous functions
+		"dot-notation": "off",													// OFF: Access properties using the dot notation (foo.bar) instead of square-bracket notation (foo["bar"])
+		"no-multi-assign": ["error", { "ignoreNonDeclaration": true }], 		// Disallow use of chained assignment expressions (const foo = bar = 0;), allows chains that don’t include initializations
+		"no-param-reassign": ["error", { "props": false}],						// Disallow reassignment of function parameters, but allow modifying properties of parameters
+		"no-plusplus": "off",													// OFF: Disallow the unary operators ++ and --
+		"no-unused-vars": "warn", 												// No unused variables (eslint:recommended gives error)
+		"prefer-destructuring": "off",											// OFF: Prefer destructuring from arrays and objects
+		"prefer-arrow-callback": [ "error", { "allowNamedFunctions": true } ] 	// Prefer arrow callbacks over anonymous functions
+	}
 }
