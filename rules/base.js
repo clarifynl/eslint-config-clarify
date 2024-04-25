cfe16523a027a6bc3714d8d01cb7bfd0ec046d59
daphnewitmer <daphne.witmer@clarify.nl>
2024-04-25 13:34:58 +0200

    add some rules + add react/recommended rulesset

--- rules/base.js
+++ rules/base.js
@@ -19,8 +19,15 @@ module.exports = {
 		"no-multi-assign": ["error", { "ignoreNonDeclaration": true }], 		// Disallow use of chained assignment expressions (const foo = bar = 0;), allows chains that don’t include initializations
 		"no-param-reassign": ["error", { "props": false}],						// Disallow reassignment of function parameters, but allow modifying properties of parameters
 		"no-plusplus": "off",													// OFF: Disallow the unary operators ++ and --
-		"no-unused-vars": "warn", 												// No unused variables (eslint:recommended gives error)
+		"no-unused-vars": "warn", 												// Warn for unused variables
 		"prefer-destructuring": "off",											// OFF: Prefer destructuring from arrays and objects
-		"prefer-arrow-callback": [ "error", { "allowNamedFunctions": true } ] 	// Prefer arrow callbacks over anonymous functions
+		"prefer-arrow-callback": [ "warn", { "allowNamedFunctions": true } ], 	// Warn to prefer arrow callbacks over anonymous functions
+		"no-use-before-define": ["error", {
+			"functions": false,
+			"classes": true,
+			"variables": true,
+			"allowNamedExports": false
+		}],																		// Allow use of functions before defined (but not variables)
+		"object-shorthand": ["error", "consistent"],							// Require consitent use of object shorthand, either all shorthands or none
 	}
 }
--- rules/react.js
+++ rules/react.js
@@ -1,9 +1,10 @@
 module.exports = {
 	"extends": [
 		"./base",
+		"plugin:react/recommended",
 		"airbnb/hooks"
 	],
-
+	"plugins": ["react"],
 	"rules": {
 		"react/react-in-jsx-scope": "off",												// OFF: Prevent missing React when using JSX
 		"react/jsx-props-no-spreading": "off",											// OFF: Enforces that there is no spreading for any JSX attribute. 
